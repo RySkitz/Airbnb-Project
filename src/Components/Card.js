f1fcdecde21bc7e7a475d005af2f29dfc9ce752b
ryze <victorfabellomain@gmail.com>
2023-08-10 09:53:33 +0800

    Mapping/Array update + Image broken01~

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,40 +1,28 @@
 import React from "react"
-import CardContent from "./CardsContent"
-import Swimmer from "../Images/swimmer.png"
-import Wedding from "../Images/wedding.png"
-import Jak from "../Images/jak.jpg"
-
+import CardData from "./CardData"
+import CardStructure from "./CardStructure"
 
 export default function Card(){
+    const CardElements = CardData.map(items => {
+        return (
+            <CardStructure
+                img = {items.img}
+                availability = {items.availability}
+                reviews = {items.reviews}
+                orders = {items.orders}
+                title = {items.title}
+                price = {items.price}
+            />
+        )
+    })
+
     return(
         <>
         <div className="cards--holder">
-            <CardContent
-                img={Swimmer}
-                availability="SOLD OUT"
-                reviews="5.0"
-                orders="(6)•USA"
-                title="Life lessons with Katie Zafarees"
-                price="From $136"
-            />
-            <CardContent
-                img={Wedding}
-                availability="AVAILABLE"
-                reviews="4.8"
-                orders="(32)•USA"
-                title="Wedding photography lessons"
-                price="From $72"
-            />
-            <CardContent
-                img={Jak}
-                availability="SOLD OUT"
-                reviews="5.0"
-                orders="(132)•PH"
-                title="Anti Selos Lessons"
-                price="From $20"
-            />
-            </div>
+            {CardElements}
+        </div>
         </>
     )
 }
+
 
---
+++ src/Components/CardData.js
@@ -0,0 +1,30 @@
+import Swimmer from "../Images/swimmer.png"
+import Wedding from "../Images/wedding.png"
+import Jak from "../Images/jak.jpg"
+
+export default [
+    {
+        img:{Swimmer},
+        availability:"SOLD OUT",
+        reviews:"5.0",
+        orders:"(6)•USA",
+        title:"Life lessons with Katie Zafarees",
+        price:"From $136"
+    },
+    {
+        img:{Wedding},
+        availability:"AVAILABLE",
+        reviews:"4.8",
+        orders:"(32)•USA",
+        title:"Wedding photography lessons",
+        price:"From $72"
+    },
+    {
+        img:{Jak},
+        availability:"SOLD OUT",
+        reviews:"5.0",
+        orders:"(132)•PH",
+        title:"Anti Selos Lessons",
+        price:"From $20"
+    }   
+]
