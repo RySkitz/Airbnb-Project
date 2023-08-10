f1b9959283744e73da161f577a824b43668af6e5
ryze <victorfabellomain@gmail.com>
2023-08-10 10:47:40 +0800

    Images public directory update

--- src/Components/CardData.js
+++ src/Components/CardData.js
@@ -1,28 +1,28 @@
 const CardData = [
     {
-        img: require("../Images/swimmer.png"),
-        availability:"SOLD OUT",
-        reviews:"5.0",
-        orders:"(6)•USA",
-        title:"Life lessons with Katie Zafarees",
-        price:"From $136"
+        img: "/images/swimmer.png",
+        availability: "SOLD OUT",   
+        reviews: "5.0",
+        orders: "(6)•USA",
+        title: "Life lessons with Katie Zafarees",
+        price: "From $136"
     },
     {
-        img: require("../Images/wedding.png"),
-        availability:"AVAILABLE",
-        reviews:"4.8",
-        orders:"(32)•USA",
-        title:"Wedding photography lessons",
-        price:"From $72"
+        img: "images/wedding.png",
+        availability: "AVAILABLE",
+        reviews: "4.8",
+        orders: "(32)•USA",
+        title: "Wedding photography lessons",
+        price: "From $72"
     },
     {
-        img: require("../Images/jak.jpg"),
-        availability:"SOLD OUT",
-        reviews:"5.0",
-        orders:"(132)•PH",
-        title:"Anti Selos Lessons",
-        price:"From $20"
+        img: "/images/jak.jpg",
+        availability: "SOLD OUT",
+        reviews: "5.0",
+        orders: "(132)•PH",
+        title: "Anti Selos Lessons",
+        price: "From $20"
     }   
-]
+];
 
-export default CardData
+export default CardData;
